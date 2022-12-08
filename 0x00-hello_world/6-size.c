d8678240a461f108ac588ab3bdd6e067d0f1db74
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-08 03:25:18 -0800

    task 6
    git commit -m task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	printf("Size of the char: %lu byte(s)\n", sizeof(char));
-	printf("Size of the char: %lu byte(s)\n", sizeof(int));
-	printf("Size of the char: %lu byte(s)\n", sizeof(long int));
-	printf("Size of the char: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of the char: %lu byte(s)\n", sizeof(float));
+	printf("Size of the int: %lu byte(s)\n", sizeof(int));
+	printf("Size of the long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of the long long long: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of the float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
