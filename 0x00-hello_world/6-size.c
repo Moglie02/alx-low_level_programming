d973f49ad1cea9dc37e1fa1e255eb5688cb3ae32
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-08 03:13:09 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of the char: %lu byte(s)\n", sizeof(char));
+	printf("Size of the char: %lu byte(s)\n", sizeof(int));
+	printf("Size of the char: %lu byte(s)\n", sizeof(long int));
+	printf("Size of the char: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of the char: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
