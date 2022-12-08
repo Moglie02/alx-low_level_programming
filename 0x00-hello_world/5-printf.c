243bf8a8eb0403db0a1313897aa2b898fabd7d4c
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-08 06:00:51 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - print a c program using printf
- 
- * Return: Always 0 (Success)
+ *
+ * Return: 0
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
