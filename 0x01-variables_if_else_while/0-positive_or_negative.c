722d1790ba5959f9c67466310e270c8abe4592b6
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-09 04:03:24 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,8 +15,8 @@ int main(void)
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
-		printf("%i is zero\n", n);
-	else if (n < 0)
 		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
 	return (0);
 }
