5cddb41d111c02f86db5c9bcdaad1d7af296d220
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-12 13:39:00 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - returns alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
