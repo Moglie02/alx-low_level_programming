46431d5f89492005b9ce65dcf09d1f185cc387fa
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-12 14:16:33 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Returns: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return(0);
+}
