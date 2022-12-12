4b2610666d2234f9be1c23e2ee3a85b3c2829f18
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-12 15:23:29 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description:prints all single digit number of base 10
+ * starting from 0, followed by a new line
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '0'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(ch);
+	return (0);
+}
