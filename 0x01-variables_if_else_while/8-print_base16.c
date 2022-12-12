436403e2d15e331c5691225d6ce507a0ca811bb9
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-12 15:32:27 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -19,6 +19,6 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	putchar(ch);
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description:
+ * Starting from 0, followed by new line
+ *
