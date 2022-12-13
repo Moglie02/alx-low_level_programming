ebc43a8013eba436089ef163d04c3a8fd645042c
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-12 16:01:44 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,6 +3,27 @@
 #include <time.h>
 /**
  * main - main block
- * Description:
+ * Description:prints all single digit number of base 10
  * Starting from 0, followed by new line
- *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+	                putchar(',');
+        	}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
