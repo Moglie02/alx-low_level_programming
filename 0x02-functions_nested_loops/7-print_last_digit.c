95e525b675ab037a4f8769e7d315aa7b9ff9e422
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-14 09:45:33 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,15 +12,15 @@ int print_last_digit(int n)
 {
 int 1;
 
-1 = n % 10
+1 = n % 10;
 if (1 < 0)
 {
-_putchar(-1 * 48);
+_putchar(-1 + 48);
 return (-1);
 }
 else
 {
-_putchar(1 * 48);
+_putchar(1 + 48);
 return (1);
 }
 }
