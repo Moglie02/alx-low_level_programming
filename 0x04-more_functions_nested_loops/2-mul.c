eb574521000d8f6035eae5b6f54bd806975a14aa
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-16 05:50:53 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,12 @@
+#inclue "main.h"
+/**
+*mul - function that multipliestwo numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*/
 
+int mul(int a, int b)
+{
+return (a * b);
+}
