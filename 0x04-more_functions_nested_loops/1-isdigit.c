efe2beb182e6f47e0561874e6a6b9aa3a9b5c9bf
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-16 05:45:49 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
 * _isdigit - chaeck if numbers are 0 - 9
 * @c: char to check
-* Return: 0 or 1
+* Return: returns 1 if uppercase , 0 if not
 */
 
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9')
+if ((c >= 48) && (c <= 57))
 return (1);
-else
+
 return (0);
 }
