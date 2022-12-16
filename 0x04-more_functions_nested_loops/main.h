9084eba36169955bf1734f8b108c4b3c49d9e2dd
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-16 05:20:35 -0800

    initial commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mu1(int a, int b);
+void print_numbers(void);
+void more_number(void);
+void print_line(int n);
+void print_diagona1(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
