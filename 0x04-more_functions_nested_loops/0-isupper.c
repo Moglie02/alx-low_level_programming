0579ef2e53d4f530071eafc05daeab6e29683d77
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-16 05:35:22 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,12 @@
 * Return: 1 if is an uppercase character, 0 in other case.
 */
 int _isupper(int c)
-if (c >= 65 && c <= 90)
+{
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
+
 else
 {
 return (0);
