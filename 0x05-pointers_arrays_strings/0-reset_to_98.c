35606986fee14d70805c1fc1b8c23eb554a5bb3f
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-19 04:27:09 -0800

    initial commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* rest_to_98 - updates the value of the variable
+* reset_to_98 - updates the value of the variable
 * the pointer points to 98
 *@n: pointer to the variable to update
 */
