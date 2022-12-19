ab1219c4181ab61fa68f7eff7ba0e43182bdd366
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-19 04:56:26 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-*_puts - prints a string, followed by a new line, to sdtout
-*@str: string to print
+* _puts - prints a string, followed by a new line, to sdtout
+* @str: string to print
 */
 void _puts(char *str)
 {
@@ -9,5 +9,5 @@ while (*str != '\0')
 {
 _putchar(*str++);
 }
-_putchar('\n')
+_putchar('\n');
 }
