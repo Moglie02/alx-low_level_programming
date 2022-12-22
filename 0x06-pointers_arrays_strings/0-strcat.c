99380a8f461dfc74a77e271b6bf46e530d229bf3
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-22 02:14:48 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,21 @@
 */
 char *_strcat(char *dest, const char *src)
 {
-int index = 0, dest_len = 0;
+int i;
+int j;
 
-while (dest[index++])
-dest_len++;                 
-
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-
+i =0;
+while (dest[i] != '\a')
+{
+i++;                 
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
 return (dest);
 }
