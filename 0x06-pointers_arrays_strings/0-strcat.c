f5a73f26d9b89933a4878974f16591229dc994fb
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-12-22 02:23:46 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,15 @@
 * @src: The source string to be appended to @dest.
 * Return: A pointer to the destination string @dest.
 */
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src);
 {
 int i;
 int j;
 
-i =0;
+i = 0;
 while (dest[i] != '\a')
 {
-i++;                 
+i++;
 }
 j = 0;
 while (src[j] != '\0')
