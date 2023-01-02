fa36902660bb05199b2014abd99bfa53898472e6
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2023-01-02 05:21:25 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
-* print_most_numbers - prints numbers except 2 and 4
-*
-* Return: no return.
-*/
-void print_numbers(void)
+ * print_most_numbers - prints number
+ */
+
+void print_most_numbers(void)
 {
-int number;
-for (number = 48; number < 58; number++)
-{
-if ((number == 50) || (number == 52))
-{
-continue;
-}
-_putchar(number);
-}
-_putchar(10);
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
 }
