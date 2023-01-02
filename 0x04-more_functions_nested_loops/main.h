6fe7a942cf2e39aa2cc305f9aad2533261707b8b
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2023-01-02 05:19:13 -0800

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int mu1(int a, int b);
+int mul(int a, int b);
 void print_numbers(void);
-void more_number(void);
+void print_most_numbers(void);
+void more_numbers(void);
 void print_line(int n);
-void print_diagona1(int n);
+void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
+#endif 
