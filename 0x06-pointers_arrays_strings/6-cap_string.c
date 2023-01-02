d5633cb22c1035e00c0d514f9c8010bf4e3a3f57
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2023-01-02 03:08:55 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,35 @@
-nclude "main.h"
+#include "main.h"
 /**
-* cap_string - Capitalizes all words of a string.
-* @str: The string to be capitalized.
-* Return: A pointer to the changed string.
-*/
-char *cap_string(char *str)
+ * cap_string - capitalizes every first letter of a word in a string
+ * separators of words are: space, tabulation
+ * new line
+ * @s: POinter to string
+ *
+ * Return: Pointer to s
+ */
+char *cap_string(char *s)
 {
-int index = 0;
+int count;
 
-while (str[index])
+count = 0;
+while (s[count] != '\0')
 {
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
-
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}' ||
-index == 0)
-str[index] -= 32;
-
-index++;
+if (s[0] >= 97 && s[0] <= 122)
+{
+s[0] = s[0] - 32;
 }
-
-return (str);
+if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+|| s[count] == '{' || s[count] == '}')
+{
+if (s[count + 1] >= 97 && s[count + 1] <= 122)
+{
+s[count + 1] = s[count + 1] - 32;
+}
+}
+count++;
+}
+return (s);
 }
